ab41bb86786275f9655ee1d6573d8ce7be8ad8c4
dac <dac@students.acts.in>
2024-12-22 13:37:12 +0530

    RandomNumbers Java program

---
+++ RandomNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        
+        
+        for (int i = 0; i < 5; i++) {
+            int randomNum = rand.nextInt(9000) + 1000; 
+            System.out.println(randomNum);
+        }
+    }
+}
